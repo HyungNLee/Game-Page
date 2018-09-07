def7c5120b585584e0f699da1671d1ee9c6c509c
Hyung Lee <hyungnaelee@gmail.com>
2018-09-07 10:26:13 -0700

    Add functions to program to make the test GetInputs() pass as true.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,6 +2,27 @@ namespace WordCounter
 {
   public class RepeatCounter
   {
-    
+    private string _word;
+    private string _sentence;
+
+    public void SetWord(string newWord)
+    {
+      _word = newWord;
+    }
+
+    public void SetSentence(string newSentence)
+    {
+      _sentence = newSentence;
+    }
+
+    public string GetWord()
+    {
+      return _word;
+    }
+
+    public string GetSentence()
+    {
+      return _sentence;
+    }
   }
 }

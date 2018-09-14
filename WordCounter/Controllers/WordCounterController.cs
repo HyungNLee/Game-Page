aca3a15f2d913504f91d025f87b17b04b65aecbb
Hyung Lee <hyungnaelee@gmail.com>
2018-09-14 09:23:57 -0700

    Write test for WordCounterController.

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+  public class WordCounterController : Controller
+  {
+    [HttpGet("/wordcounter")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
